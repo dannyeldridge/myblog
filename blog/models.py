59c68fd693b1cde8574679e973c551ad840f966e
danny eldridge <me@heardanieljames.com>
2019-04-30 22:46:25 -0700

    added markdown support

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
+import datetime
 
 class Post(models.Model):
     """docstring for Post."""
 
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    text = MarkdownxField(blank=True, null=True)
+    created_date = models.DateField(default=datetime.date.today)
+    published_date = models.DateField(blank=True, null=True)
+
+    # Create a property that returns the markdown instead
+    @property
+    def formatted_markdown(self):
+        return markdownify(self.text)
 
     def publish(self):
         self.published_date = timezone.now()
